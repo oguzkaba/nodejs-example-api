bace184b376b6e26681659635eca344599af8a9f
Oguz KABA <oguz-kaba@hotmail.com>
2022-07-29 17:08:14 +0300

    first commit

---
+++ routes/items.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const programmingLanguages = require('../services/items');
+
+/* GET programming languages. */
+router.get('/', async function(req, res, next) {
+  try {
+    res.json(await programmingLanguages.getMultiple(req.query.page));
+  } catch (err) {
+    console.error(`Error while getting items `, err.message);
+    next(err);
+  }
+});
+
+module.exports = router;
